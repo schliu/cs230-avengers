70d7c634ace6248a83f553b556bea83dde88467c
EC2 Default User <ec2-user@ip-172-31-10-139.us-east-2.compute.internal>
2019-12-09 05:56:02 +0000

    yolo-keras

--- yoloface/yoloface_avengers.py
+++ yoloface/yoloface_avengers.py
@@ -1,8 +1,15 @@
 import argparse
+import numpy as np
 import sys
 import os
+import pickle
+from keras_facenet import FaceNet
+from PIL import Image
 
 from utils import *
+
+
+IMAGE_SIZE = 182
 
 img_path = "../downloads/ageofultron/ageofultron_32.jpg"
 dir_path = "../downloads/avengers2012/avengers2012_"
@@ -47,29 +54,58 @@ def process_image(img):
     # Remove the bounding boxes with low confidence
     faces = post_process(frame, outs, CONF_THRESHOLD, NMS_THRESHOLD)
 
-    # initialize the set of information we'll displaying on the frame
-    info = [
-        ('number of faces detected', '{}'.format(len(faces)))
-    ]
+    # # initialize the set of information we'll displaying on the frame
+    # info = [
+    #     ('number of faces detected', '{}'.format(len(faces)))
+    # ]
 
-    for (i, (txt, val)) in enumerate(info):
-        text = '{}: {}'.format(txt, val)
-        cv2.putText(frame, text, (10, (i * 20) + 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, COLOR_RED, 2)
+    # for (i, (txt, val)) in enumerate(info):
+    #     text = '{}: {}'.format(txt, val)
+    #     cv2.putText(frame, text, (10, (i * 20) + 20),
+    #                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, COLOR_RED, 2)
 
-    cv2.imwrite(os.path.join(output_dir, output_file), frame.astype(np.uint8))
+    # cv2.imwrite(os.path.join(output_dir, output_file), frame.astype(np.uint8))
 
-    if (len(faces) > 0):
-        print('{} ==> # detected faces: {}'.format(img, len(faces)))
+    # if (len(faces) > 0):
+    #     print('{} ==> # detected faces: {}'.format(img, len(faces)))
 
     cap.release()
 
-    return
+    return faces
 
 def _main():
-    img_paths = generate_input_paths(dir_path, 138)
-    for ip in img_paths:
-        process_image(ip)
+    # img_paths = generate_input_paths(dir_path, 138)
+    # for ip in img_paths:
+    #     faces = process_image(ip)
+    with open(sys.argv[1], 'rb') as f:
+        (le, model) = pickle.load(f, encoding='latin1')
+
+    embedder = FaceNet()
+    directory = sys.argv[2]
+    files = sorted(os.listdir(directory))
+
+    for i, filename in enumerate(files):
+        path = directory + filename
+        boxes = process_image(path)
+        img = np.asarray(Image.open(path).convert('RGB'))
+
+        result = []
+        for box in boxes:
+            x1, y1, width, height = box
+            x1, y1 = abs(x1), abs(y1)
+            x2, y2 = x1 + width, y1 + height
+            face = img[y1:y2, x1:x2]
+            face = Image.fromarray(face).resize((IMAGE_SIZE, IMAGE_SIZE))
+            face = np.asarray(face)
+
+            rep = embedder.embeddings([face])
+            pred = model.predict_proba(rep).ravel()
+            maxI = np.argmax(pred)
+            confidence = pred[maxI]
+            person = le.inverse_transform([maxI])[0]
+            result.append('{} ({:.2f})'.format(person, confidence))
+
+        print(i, ', '.join(result))
 
 
 if __name__ == '__main__':
